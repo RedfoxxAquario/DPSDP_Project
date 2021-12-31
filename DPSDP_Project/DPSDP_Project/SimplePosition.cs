1f6caf75dbc1360cd8ac6c0ea148940ff04ddae3
Audrey CHMIEL <audrey.chmiel@edu.devinci.fr>
2021-12-31 17:48:38 +0100

    changes made to simple position and free classes

--- DPSDP_Project/DPSDP_Project/SimplePosition.cs
+++ DPSDP_Project/DPSDP_Project/SimplePosition.cs
@@ -14,12 +14,38 @@ namespace DPSDP_Project
 
         public override int onMove(Player player)
         {
-            // case 3 double dices?
+            
             Random randObj = new Random();
-            int dice1 = randObj.Next(7);
-            int dice2 = randObj.Next(7);
-            Console.WriteLine("The player's dices gave " + dice1 + " and " + dice2 + ".");
+            int dice1 = randObj.Next(1,6);
+            int dice2 = randObj.Next(1,6);
+            bool change=false;
+            Console.WriteLine(player.name + " dices gave " + dice1 + " and " + dice2 + ".");
 
+             if (dice1==dice2)//cas double
+             {
+                player.State.DoubleDiceCount ++;
+                if( player.State.StateChangeCheck() == true) //Si 3 doubles -> prison
+                {
+                    //A READAPTER POUR NOUS POUR CHANGER DE CASE ET ALLER EN PRISON
+
+                    //player.current = game.Cases[10];
+                    //Console.WriteLine("Three doubles in a row! " + player.name + " goes to jail!");
+                    //return true;
+                }
+               
+             }
+             if (!change) // sinon: il avance juste
+                {
+                    Console.Write(player.name + " goes from position " + player.current.CaseNumber + "(" + player.current.type + ") to position ");
+                    for (int i = 0; i < dice1 + dice2; i = i + 1)
+                    {
+                        player.current = player.current.next;
+                    }
+                    Console.WriteLine(player.current.CaseNumber + "(" + player.current.type + ")");
+                    player.current.stop(player, game);
+             }
+           
+            return this.CaseNumber;
             /*
             if (dice1 % 2 == 0 && dice2 % 2 == 0)//cas double
             {
